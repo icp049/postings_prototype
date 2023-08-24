e9400443e8f2bb2bca89fe5f301dbdc21f3565af
icp049 <ianpepe2000@gmail.com>
2023-08-23 23:29:47 -0600

    hardcoded post author and commentator for now

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -2,15 +2,14 @@ import React, { useState } from 'react';
 import './PostPage.css';
 import dp from '../img/dp.jpg';
 
-
 const PostPage = () => {
   const [postTitle, setPostTitle] = useState('');
   const [postText, setPostText] = useState('');
   const [comments, setComments] = useState([]);
   const [replyTexts, setReplyTexts] = useState([]);
-  
+
   // Hardcoded user information
-  const hardcodedUser = { id: 1, username: '@ianpedeglorio', photo: dp };
+  const hardcodedUser = { postAuthor: '@ianpedeglorio', commentAuthor: '@yvonnewang', photo: dp };
 
   const handleAddPost = () => {
     if (postTitle.trim() !== '' && postText.trim() !== '') {
@@ -37,8 +36,8 @@ const PostPage = () => {
         return (
           <div key={comment.id} className={`comment depth-${depth}`}>
             <div className="user-info">
-              <img src={hardcodedUser.photo}  />
-              <span>{hardcodedUser.username}</span>
+              <img src={hardcodedUser.photo} alt="User" />
+              <span>{comment.parentId === null ? hardcodedUser.postAuthor : hardcodedUser.commentAuthor}</span>
             </div>
             <h2>{comment.title}</h2>
             <p>{comment.text}</p>
